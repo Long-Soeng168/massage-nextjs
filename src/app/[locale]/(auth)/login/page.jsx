219cea289f1d15640281cdc55a9a0b200bbd02f4
Long-Soeng168 <generalpurpose167@gmail.com>
2025-02-24 16:12:40 +0700

    update

--- src/app/[locale]/(auth)/login/page.jsx
+++ src/app/[locale]/(auth)/login/page.jsx
@@ -7,7 +7,7 @@ export default function LoginPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/" className="flex items-center gap-2 font-medium">
+          <a href="#" className="flex items-center gap-2 font-medium">
             <div className="flex items-center justify-center h-10 rounded-md bg-background text-primary-foreground">
               <Image
                 width={200}
