613e9ae45447be67f100b9478001564bec909c60
Long-Soeng168 <generalpurpose167@gmail.com>
2025-04-02 12:31:33 +0700

    update

--- src/config/example-website-detail.js
+++ src/config/example-website-detail.js
@@ -1,31 +1,22 @@
 const APP_NAME = "";
-const APP_NAME_KH = "";
+const APP_NAME_KH = "U2BE Beauty Clinic";
 
-const APP_TITLE = "Helejance | Best Cosmetic Products in Phnom Penh, Cambodia - Shop Beauty & Skincare";
-const APP_DESCRIPTION = "Buy high-quality cosmetics, skincare, and beauty products at Helejance. Based in Phnom Penh, Cambodia, we offer premium beauty essentials with fast delivery and great deals. Shop now for the best in skincare and makeup!";
-const APP_KEYWORD = "cosmetics in Phnom Penh, buy beauty products Cambodia, skincare products Phnom Penh, makeup online Cambodia, best cosmetics shop Phnom Penh, beauty store Cambodia, Helejance cosmetics, premium skincare Cambodia, shop makeup online";
+const APP_TITLE = "U2BE Beauty Clinic";
+const APP_DESCRIPTION = "";
+const APP_KEYWORD = "";
 
-const APP_ADDRESS = "#123, St 456, Sangkat Toul Kork, Khan Toul Kork";
-const APP_TELEGRAM = "https://t.me/OBOCCambodia";
-const APP_EMAIL = "helejance@gmail.com";
-const APP_WEBSITE = "https://ecommerce.brolong.pro";
+const APP_ADDRESS =
+  "#41D, St777, Songkat Chrangchomres1, Khan Russey Keo , PP (At Borey Vimean Phnom Penh";
+const APP_TELEGRAM = "";
+const APP_EMAIL = "u2bestore@gmail.com";
+const APP_WEBSITE = "";
 const APP_LOGO = "/logo.png";
 const APP_LOGO_POS_80_PINTER = "/logo.png";
-const APP_FAVICON = "https://admin-ecommerce.brolong.pro/assets/images/website_infos/logo.png";
-const APP_CONTACT = "098 868 442 / 077 98 88 26";
-const APP_PAYMENT_MAYTHOD = ` <p className="text-sm text-gray-700">
-              <strong>ABA 077988826 (USD):</strong> Sok Lak
-            </p>
-            <p className="text-sm text-gray-700">
-              <strong>ABA 001210300 (KHR):</strong> Sok Lak
-            </p>
-            <p className="text-sm text-gray-700">
-              <strong>ACLEDA Toanchet 077988826 (USD & KHR)</strong>
-            </p>
-            <p className="text-sm text-gray-700">
-              <strong>Wing, True Money, Emoney ... :</strong> 077 98 88 26
-            </p>`;
-            
+const APP_FAVICON =
+  "https://admin-ecommerce.brolong.pro/assets/images/website_infos/logo.png";
+const APP_CONTACT = "+855 89 990 062";
+const APP_PAYMENT_MAYTHOD = ``;
+
 export {
   APP_NAME,
   APP_NAME_KH,
